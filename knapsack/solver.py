342466ce845f06f6b4d87467f5f17ad2f7351d33
npanj <nat.panjar@gmail.com>
2022-04-23 16:19:57 +0700

    solver: refactoring by modulize utils function

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple
-Item = namedtuple("Item", ['index', 'value', 'weight'])
+from knapsack.utils import format_input
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
-
-    # parse the input
-    lines = input_data.split('\n')
-
-    firstLine = lines[0].split()
-    item_count = int(firstLine[0])
-    capacity = int(firstLine[1])
-
-    items = []
-
-    for i in range(1, item_count+1):
-        line = lines[i]
-        parts = line.split()
-        items.append(Item(i-1, int(parts[0]), int(parts[1])))
+    items, summary_items = format_input(input_data)
 
     # a trivial algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
@@ -28,7 +14,7 @@ def solve_it(input_data):
     taken = [0]*len(items)
 
     for item in items:
-        if weight + item.weight <= capacity:
+        if weight + item.weight <= summary_items["total_capacity"]:
             taken[item.index] = 1
             value += item.value
             weight += item.weight
