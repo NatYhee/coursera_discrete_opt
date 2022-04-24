73892bcff0e2d030142f2f060ca6046b4da9987f
npanj <nat.panjar@gmail.com>
2022-04-24 17:18:33 +0700

    optimizer: adding docstring

--- knapsack/model/optimizer.py
+++ knapsack/model/optimizer.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from typing import Dict
+from typing import Dict, Tuple
 from prometheus_client import Summary
 import pyomo.environ as pyo
 
@@ -8,12 +8,23 @@ from pyomo.opt.results.results_ import SolverResults
 
 
 class LinearOptimizer:
+    """
+    Create optimizer that yeild optimal solution for knapsack problem
+    """
+
     def __init__(self, items: namedtuple, summary_items: dict) -> None:
         self.items = items
         self.summary_items = summary_items
         self.model = self.construct_model()
 
-    def construct_model(self):
+    def construct_model(self) -> pyo.ConcreteModel:
+        """
+        Constructs pyomo concrete model object including creates variable, constraints and adding objective.
+        The variable x[i] is binary variable represents whether item is selected in knapsack or not.
+
+        Returns:
+            model (pyo.ConcreteModel): concrete model object contain variables, objective and constrain.
+        """
         model = LinearOptimizer.init_concrete_model()
         model = LinearOptimizer._adding_variables(model, self.summary_items)
 
@@ -31,14 +42,32 @@ class LinearOptimizer:
         return model
 
     @staticmethod
-    def init_concrete_model():
+    def init_concrete_model() -> pyo.ConcreteModel:
+        """
+        Initiates concrete model object.
+
+        Returns:
+            model (pyo.ConcreteModel): an empty concrete model object.
+        """
         model = pyo.ConcreteModel()
         model.con = pyo.ConstraintList()
         model.obj = pyo.Objective(expr=0, sense=pyo.maximize)
         return model
 
     @staticmethod
-    def _adding_variables(model: pyo.ConcreteModel, summary_items: dict):
+    def _adding_variables(
+        model: pyo.ConcreteModel, summary_items: dict
+    ) -> pyo.ConcreteModel:
+        """
+        Adding variables to concrete model object.
+
+        Args:
+            model (pyo.ConcreteModel): an empty concrete model object.
+            summary_items (dict): a dictionary contains summary of input data.
+
+        Returns:
+            model (pyo.ConcreteModel): an concrete model object with variables.
+        """
         model.x = pyo.Var(range(summary_items["total_items"]), within=pyo.Binary)
         model.w = pyo.Var(
             range(summary_items["total_items"]), within=pyo.NonNegativeIntegers
@@ -46,7 +75,20 @@ class LinearOptimizer:
         return model
 
     @staticmethod
-    def _solve(model: pyo.ConcreteModel, solver: str = "glpk"):
+    def _solve(
+        model: pyo.ConcreteModel, solver: str = "glpk"
+    ) -> Tuple(SolverResults, dict):
+        """
+        Activate optimization process.
+
+        Args:
+            model (pyo.ConcreteModel): concrete model object contain variables, objective and constrain.
+            solver (str): name of solver that will be used in optimization process.
+
+        Returns:
+            solver_result (SolverResults): pyomo object contains summary of solver log
+            optimized_solution (dict): dictionary contain solved variable x
+        """
         opt = pyo.SolverFactory(solver)
         solver_result = opt.solve(model, tee=True)
         optimized_solution = model.x.get_values()
