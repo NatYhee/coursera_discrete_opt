50e52950d58c6d8eecee3ab11e4cf3997a51dc29
npanj <nat.panjar@gmail.com>
2022-04-24 10:33:37 +0700

    model.optimizer: complete code for linear optimizer

--- knapsack/model/optimizer.py
+++ knapsack/model/optimizer.py
@@ -6,10 +6,11 @@ import pyomo.environ as pyo
 from pyomo.core.expr.numeric_expr import SumExpression
 from pyomo.opt.results.results_ import SolverResults
 
+
 class LinearOptimizer:
-    def __init__(self, items:namedtuple, summary_items:dict) -> None:
-        self.items = items 
-        self.summary_items= summary_items
+    def __init__(self, items: namedtuple, summary_items: dict) -> None:
+        self.items = items
+        self.summary_items = summary_items
         self.model = self.construct_model()
 
     def construct_model(self):
@@ -17,33 +18,36 @@ class LinearOptimizer:
         model = LinearOptimizer._adding_variables(model, self.summary_items)
 
         total_value = sum(
-            model.x[i]*self.items[i].value for i in range(self.summary_items["total_items"])
+            model.x[i] * self.items[i].value
+            for i in range(self.summary_items["total_items"])
         )
         model.obj.expr += total_value
 
         used_capacity = sum(
-            model.x[i]*self.items[i].weight for i in range(self.summary_items["total_items"])
+            model.x[i] * self.items[i].weight
+            for i in range(self.summary_items["total_items"])
         )
         model.con.add(expr=used_capacity <= self.summary_items["total_capacity"])
         return model
-    
+
     @staticmethod
     def init_concrete_model():
         model = pyo.ConcreteModel()
         model.con = pyo.ConstraintList()
         model.obj = pyo.Objective(expr=0, sense=pyo.maximize)
         return model
-    
-    @staticmethod
-    def _adding_variables(model:pyo.ConcreteModel, summary_items:dict):
-        model.x = pyo.Var(range(summary_items["total_items"]), within=pyo.Binary)
-        model.w = pyo.Var(range(summary_items["total_items"]), within=pyo.NonNegativeIntegers)
-        return model
-    
-    @staticmethod
-    def _solve(model:pyo.ConcreteModel, solver:str="glpk"):
-        opt = pyo.SolverFactory('glpk')
-        result = opt.solve(model)
-        return result
 
-    
+    @staticmethod
+    def _adding_variables(model: pyo.ConcreteModel, summary_items: dict):
+        model.x = pyo.Var(range(summary_items["total_items"]), within=pyo.Binary)
+        model.w = pyo.Var(
+            range(summary_items["total_items"]), within=pyo.NonNegativeIntegers
+        )
+        return model
+
+    @staticmethod
+    def _solve(model: pyo.ConcreteModel, solver: str = "glpk"):
+        opt = pyo.SolverFactory(solver)
+        solver_result = opt.solve(model, tee=True)
+        optimized_solution = model.x.get_values()
+        return solver_result, optimized_solution
