7eb3a286dac1443de0b16634a7f3112ad5d8da3e
npanj <nat.panjar@gmail.com>
2022-05-06 15:39:54 +0700

    utils: adding get_edges_connection_array function

--- coloring/utils.py
+++ coloring/utils.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+import numpy as np
 
 
 def format_input(input_data: str) -> Tuple[list, dict]:
@@ -9,7 +10,7 @@ def format_input(input_data: str) -> Tuple[list, dict]:
         input_data (str): string of input data extracted from the raw file.
 
     Returns:
-        edges (namedtuple): named tuple with two elements which its first element is source node and the second element is target node.
+        edges (list): list of tuple contains information of source node, first index of tuple, and target node, second element of tuple.
         summary_dict (dict): dictionary contains summary information of number of node and number of edges.
     """
 
@@ -31,5 +32,21 @@ def format_input(input_data: str) -> Tuple[list, dict]:
     return edges, summary_dict
 
 
-def get_edges_connection_array(edges: list):
-    pass
+def get_edges_connection_array(edges: list, summary_edges: dict) -> np.ndarray:
+    """
+    Create metrix size total_node*total_node which represent the conection between node[i], row, to node[j], column.
+
+    Args:
+        edges (list): list of tuple contains information of source node, first index of tuple, and target node, second element of tuple.
+        summary_edges (dict): dictionary contains summary information of number of node and number of edges.
+
+    Returns:
+        edges_connection_array (np.ndarray): two dimension binary array which row represent source node and column represent destination node.
+    """
+    total_node = summary_edges["total_node"]
+    edges_connection_array = np.zeros((total_node, total_node))
+
+    for edge in edges:
+        edges_connection_array[edge[0]][edge[1]] = int(1)
+
+    return edges_connection_array
